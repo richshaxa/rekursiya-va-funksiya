233caa0041b1c6cb01d9bedecf05316fcf46eb88
richshaxa <165869637+richshaxa@users.noreply.github.com>
2024-04-03 09:24:36 +0500

    Add files via upload

---
+++ 42#dars.py
@@ -0,0 +1,95 @@
+# a = 3
+# b = 5
+
+# def add():
+#     a = 2
+#     b = 1
+#     print(a+b)
+# add() # 3
+# print(a+b) # 8
+# dollar = 100
+# def uzbekistan():
+#     sum = 1000
+#     print(dollar)
+#     print(sum)
+
+# uzbekistan()
+# print(dollar)
+# print(sum)
+# a = 3
+# b = 6
+# def func(a,b):
+#     print(a,b)
+    
+# func(a,b)
+# ism = 'zafar'
+# def name():
+#     ism = "ali"
+#     print(ism)
+# name()
+# print(ism)
+# # rekursa
+# def full():
+#     print("Hello")
+#     def name():
+#         print("Zafar")
+#     name()
+# full()
+# def rec():
+#     print("a")
+# b = 'Hovoni isib ketishi'
+# def Chine():
+#     print(b)
+#     a = 'Kitob'
+#     for i in range(10):
+#         print(i)
+# a = "Kitob"
+# def Uzbek():
+#     print(a)
+#     print(b)
+#     for i in range(100):
+#         print(i)
+
+# def Us():
+#     print(a)
+
+# def chine():
+#     print("Hello")
+#     def byd():
+#         print("Mashinalar")
+#     byd()
+# chine()
+# def add():
+#         a = 4
+#         b = 5
+#         print(a+b)
+# def a():
+#     add()
+#     print("a klass haqida malumot")
+
+# def b():
+#     add()
+#     print("b klass haqida malumot")
+# a()
+# b()
+# def father():
+#     s = 1
+#     for i in range(1,11):
+#         s*=i
+#     print(s)
+     
+# def child1():
+#     print("1 dan 10 gacha sonalarni bir biriga kupaytir")
+#     father()
+# child1()  
+
+# def child2(n):
+#     if n==1:
+#         return 1
+#     else: 
+#         return n*child2(n-1)
+# print(child2(5))
+# n = 5  =  5*child2(4)
+# n = 4  = 4*child2(3)
+# n = 3 = 3*child2(2)
+# n = 2 = 2*child2(1)
